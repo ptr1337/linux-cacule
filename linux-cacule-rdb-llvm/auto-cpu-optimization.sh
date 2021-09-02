498347c4d6d8dc4a4c3e8182007dee21873a6e25
P.Jung <ptr1337@cachyos.org>
2021-09-02 21:03:13 +0000

    ntfs3 & cacule

--- linux-cacule-rdb-llvm/auto-cpu-optimization.sh
+++ linux-cacule-rdb-llvm/auto-cpu-optimization.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
 #######################################
-#        AUTO-CPU-OPTIMIZATION        #
-#######################################
-#      CREATOR : BL4CKH47H4CK3R       #
-#######################################
-#  HTTPS://GITHUB.COM/BL4CKH47H4CK3R  #
+#      Creator : BL4CKH47H4CK3R       #
+#  https://github.com/BL4CKH47H4CK3R  #
 #######################################
 
-CPU=`gcc -Q -march=native --help=target | grep march | awk '{print $2}' | head -1`
-MARCH=`echo ${CPU} | tr '[:lower:]' '[:upper:]'`&& echo
+echo -e "-------------------------"
+echo -e "| Auto-CPU-Optimization |"
+echo -e "-------------------------"
+echo -e "[1] Enable CPU Optimization"
+echo -e "[2] Disable CPU Optimization"
 
-if [[ ${MARCH} == "ZNVER" ]]
-then
-	MARCH="ZEN"
+read -p "[*] Enter Choice [1, 2]: " INPUT
 
-elif [[ ${MARCH} == "ZNVER2" ]]
+if [[ ${INPUT} == 1 ]]
 then
-	MARCH="ZEN2"
+	CPU=`gcc -Q -march=native --help=target | grep march | awk '{print $2}' | head -1`
+	MARCH=`echo "${CPU}" | tr '[:lower:]' '[:upper:]'`
 
-elif [[ ${MARCH} == "ZNVER3" ]]
-then
-	MARCH="ZEN3"
+	if [[ ${MARCH} == "ZNVER" ]]
+	then
+		MARCH="ZEN"
+
+	elif [[ ${MARCH} == "ZNVER2" ]]
+		then
+		MARCH="ZEN2"
+
+	elif [[ ${MARCH} == "ZNVER3" ]]
+	then
+		MARCH="ZEN3"
+	fi
+
+	echo -e "[*] Detected CPU : `echo ${MARCH}`"
+	echo -e "[*] Setting Detected CPU Microarchitecture ..."
+	sed -i '/CONFIG_GENERIC_CPU=y/d' .config
+	sed -i "s/\# CONFIG_M${MARCH} is not set/CONFIG_M${MARCH}=y/g" .config
 fi
-
-echo "----------------------------------"
-echo "| APPLYING AUTO-CPU-OPTIMIZATION |"
-echo "----------------------------------"
-echo "[*] DETECTED CPU (MARCH) : ${MARCH}"
-sed -i "/CONFIG_GENERIC_CPU=y/d;s/\# CONFIG_M${MARCH} is not set/CONFIG_M${MARCH}=y/g" *config*
-sleep 3 && echo
